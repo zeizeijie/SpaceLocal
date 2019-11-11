22b5ad4bd36e68adfb2b0c379749b933f7824ab8
zengzhijun <595418670@qq.com>
2019-11-11 16:28:20 +0800

    gengxing

--- Workspace/HellowDjango-blog-tutorial/blogproject/settings/common.py
+++ Workspace/HellowDjango-blog-tutorial/blogproject/settings/common.py
@@ -13,19 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^&p0_8#_pwez6v1re^o32!ikfhjj4h#qi+t^!d4_bbtok(ub-0'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zeizeijie.club']
 
 
 # Application definition
@@ -121,4 +115,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
